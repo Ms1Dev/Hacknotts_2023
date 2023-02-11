64a5036369cb3b5e7025963722826fe1b55f7101
Michael Swann <n1020368@my.ntu.ac.uk>
2023-02-11 19:32:46 +0000

    initial commit

---
+++ src/main/java/com/gluonapplication/GluonApplication.java
@@ -0,0 +1,44 @@
+package com.gluonapplication;
+
+import com.gluonapplication.views.PrimaryView;
+import com.gluonapplication.views.SecondaryView;
+import com.gluonhq.charm.glisten.application.AppManager;
+import com.gluonhq.charm.glisten.visual.Swatch;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import static com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW;
+
+public class GluonApplication extends Application {
+
+    public static final String PRIMARY_VIEW = HOME_VIEW;
+    public static final String SECONDARY_VIEW = "Secondary View";
+
+    private final AppManager appManager = AppManager.initialize(this::postInit);
+
+    @Override
+    public void init() {
+        appManager.addViewFactory(PRIMARY_VIEW, () -> new PrimaryView().getView());
+        appManager.addViewFactory(SECONDARY_VIEW, () -> new SecondaryView().getView());
+
+        DrawerManager.buildDrawer(appManager);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        appManager.start(primaryStage);
+    }
+
+    private void postInit(Scene scene) {
+        Swatch.BLUE.assignTo(scene);
+
+        scene.getStylesheets().add(GluonApplication.class.getResource("style.css").toExternalForm());
+        ((Stage) scene.getWindow()).getIcons().add(new Image(GluonApplication.class.getResourceAsStream("/icon.png")));
+    }
+
+    public static void main(String args[]) {
+        launch(args);
+    }
+}
