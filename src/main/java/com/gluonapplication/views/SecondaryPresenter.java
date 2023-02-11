e9356f04fa573578f2e0b0dae4a923a8a3552037
Michael Swann <n1020368@my.ntu.ac.uk>
2023-02-11 22:02:50 +0000

    Push painting changes

--- src/main/java/com/gluonapplication/views/SecondaryPresenter.java
+++ src/main/java/com/gluonapplication/views/SecondaryPresenter.java
@@ -3,25 +3,47 @@ package com.gluonapplication.views;
 import com.gluonhq.charm.glisten.animation.BounceInRightTransition;
 import com.gluonhq.charm.glisten.application.AppManager;
 import com.gluonhq.charm.glisten.control.AppBar;
-import com.gluonhq.charm.glisten.control.TextField;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class SecondaryPresenter {
+    static final int CIRCLE_RAD = 25;
 
     @FXML
     private View secondary;
     @FXML
     Canvas canvas;
+    @FXML
+    Pane mainPane;
+    
+    GraphicsContext graphics;
 
 
     public void initialize() {
         secondary.setShowTransitionFactory(BounceInRightTransition::new);
 
         canvas.setCursor(Cursor.CROSSHAIR);
+        canvas.widthProperty().bind(mainPane.widthProperty());
+        canvas.heightProperty().bind(mainPane.heightProperty());
+        
+        graphics = canvas.getGraphicsContext2D();
+        graphics.setFill(Color.CHOCOLATE);
+           
+        canvas.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                graphics.fillOval((me.getX() - CIRCLE_RAD),(me.getY() - CIRCLE_RAD), (2*CIRCLE_RAD), (2*CIRCLE_RAD));
+                
+            }
+        });
         
         ScoreCounter counter = new ScoreCounter();
         
@@ -37,5 +59,28 @@ public class SecondaryPresenter {
                         counter.increaseIncrement(0.2)));
             }
         });
+    }
+    
+            
+    private boolean isPainted() {
+        boolean painted = false;
+            double x = canvas.getLayoutX();
+            double y = canvas.getLayoutY();
+            
+            int xPixels = (int) (canvas.getWidth() / CIRCLE_RAD);
+            int yPixels = (int) (canvas.getHeight() / CIRCLE_RAD);
+            
+            for(int i = 0; i < xPixels; i++) {
+                x += CIRCLE_RAD;
+                for(int j = 0; j < yPixels; j++) {
+                    y += CIRCLE_RAD;
+//                    WritableImage image = canvas.s
+                }
+            }
+            // for pixels x
+                // for pixels y
+                    // painted = pixel at x,y has colour 
+        
+        return painted;
     }
 }
